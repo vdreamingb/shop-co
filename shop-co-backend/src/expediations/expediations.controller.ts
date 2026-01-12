5dbb3f722ae379866d1bc40c537824cc0bbf209f
vdreamingb <vdvorschii@gmail.com>
2026-01-12 11:56:37 +0200

    Swagger documentation added and expendiation endpoints

--- shop-co-backend/src/expediations/expediations.controller.ts
+++ shop-co-backend/src/expediations/expediations.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { ExpediationsService } from './expediations.service';
+import { JwtGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RoleEnum } from 'src/common/enums/roles.enum';
+import { CreateExpediationsDto } from './dto/createExped.dto';
 
 @Controller('expediations')
 export class ExpediationsController {
   constructor(private readonly expediationsService: ExpediationsService) {}
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(RoleEnum.ADMIN)
+  @Get('all')
+  async getAllExpediations() {
+    return this.expediationsService.all();
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('create/:userId')
+  async createExpediation(@Param('userId', ParseIntPipe) userId: number, @Body() data: CreateExpediationsDto) {
+    return this.expediationsService.create(userId,data);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get(':expeditionId')
+  async getExpediationById(@Param('expeditionId', ParseIntPipe) expeditionId: number) {
+    return this.expediationsService.getByExpediationId(expeditionId);
+  }
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(RoleEnum.ADMIN)
+  @Get('user/:userId')
+  async getExpediationsByUserId(@Param('userId', ParseIntPipe) userId: number) {
+    return this.expediationsService.getByUserId(userId);
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('modifyStatus/:id')
+  async modifyStatus(@Param('id', ParseIntPipe) id: number, @Body() body: {status: boolean}) {
+    return this.expediationsService.modifyStatus(id, body.status)
+  }
 }
--- shop-co-backend/src/main.ts
+++ shop-co-backend/src/main.ts
@@ -1,20 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import "dotenv/config"
-import { ConsoleLogger } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import "dotenv/config";
+import { ConsoleLogger } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useLogger(new ConsoleLogger(
-    {
-      prefix: 'Shop-co-backend'
-    }
-  ));
+  const config = new DocumentBuilder()
+    .setTitle("Shop-co endpoints")
+    .setDescription("Shop-co website backend enpoints")
+    .setVersion("1.0")
+    .addTag("shop-co")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+  app.useLogger(
+    new ConsoleLogger({
+      prefix: "Shop-co-backend",
+    })
+  );
   app.use(cookieParser());
   app.use(helmet());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
