7c00e4ebe93ae306d061ef665fe2129ed98a6112
vdreamingb <vdvorschii@gmail.com>
2026-03-05 17:22:43 +0200

    Sign up and log in + adaptive home page started profile page

--- shop-co-frontend/src/app/(auth)/signup/page.tsx
+++ shop-co-frontend/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
 import SignUpForm from "@/components/forms/signUpForm";
 
-export default function SignUpPage(){
-    return <>
-        <h1 className="integral font-medium text-4xl text-center">Sign up</h1>
-        <SignUpForm />
+export default function SignUpPage() {
+  return (
+    <>
+      <h1 className="integral font-medium text-4xl text-center">Sign up</h1>
+      <SignUpForm />
     </>
+  );
 }
