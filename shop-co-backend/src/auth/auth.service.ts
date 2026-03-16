4c7d981a4ef0d749854907b4ca3ae8d6bd009c27
vdreamingb <vdvorschii@gmail.com>
2026-03-16 11:35:41 +0500

    Changed some settings in backend

--- shop-co-backend/src/auth/auth.service.ts
+++ shop-co-backend/src/auth/auth.service.ts
@@ -123,7 +123,7 @@ export class AuthService {
     res.clearCookie("accessToken");
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       maxAge: 1000 * 60 * 7,
       sameSite: "lax",
       path: "/",
@@ -131,7 +131,7 @@ export class AuthService {
     res.clearCookie("refreshToken");
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 30,
       sameSite: "lax",
       path: "/",
--- shop-co-frontend/src/config/axios.config.ts
+++ shop-co-frontend/src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1110/api/",
+    baseURL: "https://shop-co-pi-nine.vercel.app/api/",
     timeout: 10000,
     withCredentials: true,
 })
